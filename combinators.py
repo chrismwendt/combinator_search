5c1077746c3b82ca89be791116fde0892947d38e
Chris Wendt <chrismwendt@gmail.com>
2013-08-26 18:33:03 -0400

    Used itertools.product instead of nested for loops

--- combinators.py
+++ combinators.py
@@ -14,22 +14,17 @@ def applications(arguments=0, tree=None):
         for argument in range(arguments):
             yield argument
     else:
-        for left in applications(arguments=arguments, tree=tree[0]):
-            for right in applications(arguments=arguments, tree=tree[1]):
-                yield [left, right]
+        for left, right in itertools.product(applications(arguments=arguments, tree=tree[0]), applications(arguments=arguments, tree=tree[1])):
+            yield [left, right]
 
 def binary_trees(leaves=None):
     if leaves is None:
         for leaves in itertools.count(1):
             for tree in binary_trees(leaves=leaves):
                 yield tree
-
     if leaves == 1:
         yield None
     else:
         for i in range(1, leaves):
-            leaves_left = i
-            leaves_right = leaves-i
-            for tree_left in binary_trees(leaves=leaves_left):
-                for tree_right in binary_trees(leaves=leaves_right):
-                    yield [tree_left, tree_right]
+            for left, right in itertools.product(binary_trees(leaves=i), binary_trees(leaves=leaves-i)):
+                yield [left, right]
